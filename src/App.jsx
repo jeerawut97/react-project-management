dc30b63b31cbfc742b56c70024cf670c9a823512
JRW <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1@github.com>
2023-12-11 12:44:05 +0700

    Adding Component New Projects & Reusable Component Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProjects from "./components/NewProjects";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProjects />
     </main>
   );
 }
