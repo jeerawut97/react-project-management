0c6562b7bcc422a1ef265410ac547fbfff61bad8
jee <jeerawut.jd40@gmail.com>
2023-12-24 13:22:42 +0700

    hot fix dueDate

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -25,7 +25,7 @@ export default function NewProject({ onAdd, onCancel }) {
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
-            enteredDescription: enteredDueDate
+            dueDate: enteredDueDate
         });
     }
 
